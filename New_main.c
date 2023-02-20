efaed05afd502945aca299348115800d82a01aac
Arnaud DURAND <arnduran@e2r1p12.clusters.42paris.fr>
2023-02-20 22:43:27 +0100

    normed 90%

--- New_main.c
+++ New_main.c
@@ -8,90 +8,88 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-void  print_tab(char **tab)
-{
-	 int  i;
+//cree un fichier av open then lire et envoyer le fichier
 
-	 i = 0;
-	 while (tab[i])
-			{
-				printf("%s\n", tab[i]);
-				i++;
-			}
+void	print_tab(char **tab)
+{
+	int	i;
+
+	i = 0;
+	while (tab[i])
+	{
+		printf("%s\n", tab[i]);
+		i++;
+	}
 }
 
-void    exec_pipe(char *env[])
+void	exec_pipe(char *env[])
 {
-		int pid;
-		int pipefd[2];
-		int fd_in;
-		int i;
-		int nb_pipe; // a compter dans le parser || nb_pipe = nb_command - 1;
-		char  cmd[] = "/usr/bin/ls";
-		char  *arvec[] = {"ls", "-l", NULL};
-		char  *envec[] = {NULL};
+	int	pid;
+	int	pipefd[2];
+	int	fd_in;
+	int	i;
+	int	nb_pipe;
 
-		nb_pipe = 2;
-		i = 0;
-		fd_in = dup(STDIN_FILENO);
-		while (i < nb_pipe)
+	nb_pipe = 2;
+	i = 0;
+	fd_in = dup(STDIN_FILENO);
+	while (i < nb_pipe)
+	{
+		pipe(pipefd);
+		pid = fork();
+		pipefd[1] = dup2(fd_in, STDIN_FILENO);
+		if (pid == 0)
 		{
-			pipe(pipefd); // je dois access() avant de read()
-			pid = fork();
-			pipefd[1] = dup2(fd_in, STDIN_FILENO);
-			if (pid == 0) // pid == 0 -> dans l'enfant
-			{
-				if (i != 0)
-					dup2(fd_in, STDIN_FILENO);
-				if (i + 1 != nb_pipe)
-					dup2(pipefd[1], STDOUT_FILENO);
-				close(fd_in);
-				close(pipefd[0]);
-				close(pipefd[1]);
-				if (i == 0)
-					execve(cmd, arvec, envec);
-			}
-			dup2(pipefd[0], fd_in);
+			if (i != 0)
+				dup2(fd_in, STDIN_FILENO);
+			if (i + 1 != nb_pipe)
+				dup2(pipefd[1], STDOUT_FILENO);
+			close(fd_in);
 			close(pipefd[0]);
 			close(pipefd[1]);
-			i++;
+			//if (i == 0)
+			//	execve(cmd, arvec, envec);
 		}
-		close(fd_in);
-		
+		dup2(pipefd[0], fd_in);
+		close(pipefd[0]);
+		close(pipefd[1]);
+		i++;
+	}
+	close(fd_in);
 }
 
-char  **extract_path(char **env)
+char	**extract_path(char **env)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (env[i] && ft_strncmp("PATH=", env[i], 5))
 		i++;
-	return(ft_split(env[i] + 5, ':'));
+	return (ft_split(env[i] + 5, ':' ));
 }
 
-char  *find_binaries(char **tab, char *bin_name)
+char	*find_binaries(char **tab, char *bin_name)
 {
-	int   i;
-	char  *s;
+	int		i;
+	char	*s;
 
 	i = 0;
 	while (tab[i])
 	{
 		s = ft_strjoin3(tab[i], "/", bin_name);
-			if (access(s, X_OK) == 0)
-				return (s);
+		if (access(s, X_OK) == 0)
+			return (s);
 		i++;
 		free(s);
 	}
 	return (NULL);
 }
 
-void  exec_commands(char **env, char **tab, char *argv)
+void	exec_commands(char **env, char **tab, char *argv)
 {
-	char  **avtab;
-	int   i;
-	char  *s;
+	char	**avtab;
+	int		i;
+	char	*s;
 
 	i = 0;
 	//printf("argv = %s\n", argv);
@@ -106,12 +104,12 @@ void  exec_commands(char **env, char **tab, char *argv)
 	exit(EXIT_FAILURE);
 }
 
-int exec(char **env, char **argv, int argc, char **tab)
+int	exec(char **env, char **argv, int argc, char **tab)
 {
 	int		pipefd[2];
 	int		fd_in;
 	int		i;
-	int   nb_commands;
+	int		nb_commands;
 
 	nb_commands = argc - 3;
 	i = 0;
@@ -136,10 +134,11 @@ int exec(char **env, char **argv, int argc, char **tab)
 			if ((i + 1) != nb_commands)
 				dup2(pipefd[1], STDOUT_FILENO);
 			else
-				{
-					fd_in = open(argv[argc - 1],  O_CREAT | O_WRONLY | O_TRUNC, 0644);
-					dup2(fd_in, STDOUT_FILENO);
-				}
+			{
+				fd_in = open(argv[argc - 1], O_CREAT | O_WRONLY
+				|O_TRUNC, 0644);
+				dup2(fd_in, STDOUT_FILENO);
+			}
 			close(pipefd[0]);
 			close(pipefd[1]);
 			close(fd_in);
@@ -157,17 +156,17 @@ int exec(char **env, char **argv, int argc, char **tab)
 	return (0);
 }
 
-int main(int argc, char *argv[], char **env)
+int	main(int argc, char *argv[], char **env)
 {
-		char **tab;
-		int   i;
+	char	**tab;
+	int		i;
 
 	i = 0;
-		if (argc < 5)
-			exit((printf("arg < 5"), 0));
-		tab = extract_path(env);
-		if (!(tab))
-			return (0);
-		exec(env, argv, argc, tab);
+	if (argc < 5)
+		exit((printf("arg < 5"), 0));
+	tab = extract_path(env);
+	if (!(tab))
 		return (0);
+	exec(env, argv, argc, tab);
+	return (0);
 }
